ef17100a8affe34d8eeda09060151387641723c0
bramble100 <arnold.barna@gmail.com>
2017-09-28 12:27:25 +0200

    feat(Task): new class added

---
+++ ToDoApplication/ToDoApplication/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApplication
+{
+    class Task
+    {
+        public int ID;
+        public string Description;
+    }
+}
